03fe9f9abb29a63ed74fdac2df63e9be70439018
MisterPerfect007 <leneodepart@gmail.com>
2023-01-27 10:08:09 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests, json
+
+url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
+def main():
+    resp = requests.get(url).json()
+    return json.dumps(resp)
+
+main()
